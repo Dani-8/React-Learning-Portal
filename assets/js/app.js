be1076c31efa1cc149254f873f648ded90cb14db
Dani-8 <daniyalw6190@gmail.com>
2026-02-09 00:03:57 +0500

    update

--- assets/js/app.js
+++ assets/js/app.js
@@ -3,7 +3,18 @@
 // }
 
 
+// function toggleTheme() {
+//     const isLight = document.documentElement.classList.toggle('light-mode');
+//     localStorage.setItem('theme', isLight ? 'light' : 'dark');
+//     updateIcon(isLight);
+// }
 
+// function updateIcon(isLight) {
+//     const sunPath = "M12 3v1m0 16v1m9-9h-1M4 12H3m15.364-6.364l-.707.707M6.343 17.657l-.707.707m12.728 0l-.707-.707M6.343 6.343l-.707-.707M12 8a4 4 0 100 8 4 4 0 000-8z";
+//     const moonPath = "M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z";
+//     document.getElementById('sun-path').setAttribute('d', isLight ? moonPath : sunPath);
+// }
+// updateIcon(document.documentElement.classList.contains('light-mode'));
 
 
 
