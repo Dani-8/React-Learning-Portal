f72fd874117edc72f1d06145746576ba80671d33
Dani-8 <daniyalw6190@gmail.com>
2026-02-22 08:40:38 +0500

    update

--- Concepts Directory/React Hooks/app.js
+++ Concepts Directory/React Hooks/app.js
@@ -1,4 +1,19 @@
-
+window.addEventListener('scroll', () => {
+    const sections = document.querySelectorAll('.hook-section')
+    const navLinks = document.querySelectorAll('.nav-link')
+    let current = "";
+    sections.forEach(section => {
+        if (window.pageYOffset >= section.offsetTop - 250) {
+            current = section.getAttribute('id')
+        }
+    })
+    navLinks.forEach(link => {
+        link.classList.remove('active')
+        if (link.getAttribute('href') === `#${current}`) {
+            link.classList.add('active')
+        }
+    })
+})
 
 
 
